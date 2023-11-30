5b152b56b6f9bf5d161ff3ed46f3b18a576cea51
danthor11 <danieljpb11@gmail.com>
2023-11-29 22:06:17 -0400

    fix: cambio de jefe

--- src/pages/consejo-comunal/[id]/censo/create/index.tsx
+++ src/pages/consejo-comunal/[id]/censo/create/index.tsx
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router";
 import { LayoutContent } from "~/components/Layout";
 import { GreatForm } from "~/components/censo/GreatForm";
-import { JefeForm } from "~/components/censo/JefeForm";
 
 const CreateCenso = () => {
   const router = useRouter();
@@ -10,7 +9,6 @@ const CreateCenso = () => {
     <LayoutContent>
       <div className="container mx-auto max-w-2xl p-8">
         <GreatForm consejoComunalId={consejoComunalId} />
-        {/* <JefeForm consejoComunalId={consejoComunalId} /> */}
       </div>
     </LayoutContent>
   );
